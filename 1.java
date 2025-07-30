8cc75a1a1338c33b3b50c15cc40ae64785ee8e6e
seobeomju <alt10063@naver.com>
2025-07-30 16:20:30 +0900

    feat: 클론 매서드 추가

--- 1.java
+++ 1.java
@@ -6,9 +6,16 @@ class Main{
         System.out.println(Arrays.toString(solution(new int[] {1,-5,2,4,3})));
         System.out.println(Arrays.toString(solution(new int[] {2,1,1,3,2,5,4})));
         System.out.println(Arrays.toString(solution(new int[] {6,1,7})));
+
     }
+    // public static int[] solution(int[] arr){
+    //     Arrays.sort(arr);
+    //     return arr;
+    // }
     public static int[] solution(int[] arr){
-        Arrays.sort(arr);
-        return arr;
+        int[] clone = arr.clone();
+        Arrays.sort(clone);
+        return clone;
     }
+    
 }
